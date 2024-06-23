3425d5a13a200bbfe0cc250db4749069ce096069
ronaldophc <camposr@alunos.utfpr.edu.br>
2024-06-23 20:10:32 -0300

    Validation in register user

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -11,10 +11,24 @@ export class RegisterComponent {
   email: string = '';
   name: string = '';
   password: string = '';
+  errorMessage = '';
 
   constructor(private authService: AuthService, private router: Router) { }
 
   onSubmit() {
+    this.errorMessage = '';
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(this.email)) {
+      this.errorMessage = 'Email invalido';
+      return;
+    }
+    if(this.password.length < 6) {
+      this.errorMessage = 'A senha deve conter no minímo 6 caracteres';
+      return;
+    }
+    if(this.name.length < 3) {
+      this.errorMessage = 'O nome deve conter no minímo 3 caracteres';
+      return;
+    }
     if (this.authService.register(1, this.email, this.name, this.password)) {
       this.router.navigate(['/login']);
     }
